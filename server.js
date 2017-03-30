3ddffbdde5aef009b847d5af30be619daa7ce5b7
nalcus <nalcus@gmail.com>
2017-03-30 11:37:45 +0000

    first working version

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(function(req, res) {
 
-    res.send("header info goes here")
+    var ip=req.headers["x-forwarded-for"]
+    var lang=req.headers["accept-language"]
+    lang.split(',', 1)[0]
+    var soft=req.headers["user-agent"]
+    soft = soft.split(')',1).pop().trim();
+    soft = soft.split('(').pop().trim();
+    res.json({ipaddress:ip, language:lang, software:soft})
 })
 
 app.listen(app.get('port'), function() {
